3ac796af5d8f2923fbb76bca1c773ec57aba0e4b
DrJuanch <juanjo88023@gmail.com>
2023-02-10 13:04:34 -0500

    Corrección base de datos

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ const app = express();
 
 const todoRoutes = require("./routes/todoRoutes");
 
-const connectionOptions = {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false};
+/* const connectionOptions = {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false}; */
 
-app.user(express.json());
+app.use(express.json()); // a method to send data 
 
 mongoose.set('strictQuery', false);
 
@@ -17,7 +17,7 @@ const password = 'Admin1342*';
 const dbname = 'toDoApp';
 
 const url = `mongodb+srv://${user}:${password}@todolist.xxqocxq.mongodb.net/${dbname}?retryWrites=true&w=majority`;
-mongoose.connect(url,connectionOptions).then(()=>console.log("Conexion exitosa")).catch((err)=> console.error(err));
+mongoose.connect(url).then(()=>console.log("Conexion exitosa")).catch((err)=> console.error(err));
 
 app.use("/toDos", todoRoutes);
 
